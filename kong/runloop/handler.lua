8559b0d3582b06ad0bcbc297b1df8d3c68a41e1f
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2020-11-09 17:55:18 +0200

    chore(handler) use kong.async instead of ngx.timer.*
    ### Summary
    
    This updates `kong.runloop.handler` to use `kong.async` instead
    of `ngx.timer` api.

--- kong/runloop/handler.lua
+++ kong/runloop/handler.lua
@@ -32,8 +32,6 @@ local log          = ngx.log
 local exit         = ngx.exit
 local null         = ngx.null
 local header       = ngx.header
-local timer_at     = ngx.timer.at
-local timer_every  = ngx.timer.every
 local subsystem    = ngx.config.subsystem
 local clear_header = ngx.req.clear_header
 local unpack       = unpack
@@ -974,9 +972,10 @@ return {
       end
 
       -- initialize balancers for active healthchecks
-      timer_at(0, function()
-        balancer.init()
-      end)
+      local ok, err = kong.async:run(balancer.init)
+      if not ok then
+        kong.log.err("failed to initialize balancer: ", err)
+      end
 
       local strategy = kong.db.strategy
 
@@ -1022,7 +1021,7 @@ return {
           on_timeout = "return_true",
         }
 
-        timer_every(worker_state_update_frequency, function(premature)
+        kong.async:every(worker_state_update_frequency, function(premature)
           if premature then
             return
           end
@@ -1043,7 +1042,7 @@ return {
           on_timeout = "return_true",
         }
 
-        timer_every(worker_state_update_frequency, function(premature)
+        kong.async:every(worker_state_update_frequency, function(premature)
           if premature then
             return
           end
